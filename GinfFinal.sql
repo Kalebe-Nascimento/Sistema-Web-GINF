1544df9f37aebc375640d3f9ba2e2e4e9620ce70
PlayerUnnamed <39560664+PlayerUnnamed@users.noreply.github.com>
2019-09-25 12:22:26 -0300

    Update GinfFinal.sql

--- GinfFinal.sql
+++ GinfFinal.sql
@@ -1,11 +1,11 @@
-﻿--
+--
 -- PostgreSQL database dump
 --
 
 -- Dumped from database version 9.5.7
 -- Dumped by pg_dump version 9.5.7
 
--- Started on 2019-09-25 09:11:47 BRT
+-- Started on 2019-09-25 12:20:24 BRT
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -24,7 +24,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2148 (class 0 OID 0)
+-- TOC entry 2158 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -39,7 +39,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 182 (class 1259 OID 49491)
+-- TOC entry 181 (class 1259 OID 25517)
 -- Name: admin; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -54,7 +54,23 @@ CREATE TABLE admin (
 ALTER TABLE admin OWNER TO postgres;
 
 --
--- TOC entry 181 (class 1259 OID 49486)
+-- TOC entry 183 (class 1259 OID 25539)
+-- Name: postagem; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE postagem (
+    id_post integer NOT NULL,
+    titulo character varying(100),
+    texto text,
+    data_hora timestamp without time zone,
+    autor character varying(50)
+);
+
+
+ALTER TABLE postagem OWNER TO postgres;
+
+--
+-- TOC entry 182 (class 1259 OID 25520)
 -- Name: usuario; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -69,20 +85,39 @@ CREATE TABLE usuario (
 ALTER TABLE usuario OWNER TO postgres;
 
 --
--- TOC entry 2140 (class 0 OID 49491)
--- Dependencies: 182
+-- TOC entry 2148 (class 0 OID 25517)
+-- Dependencies: 181
 -- Data for Name: admin; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+COPY admin (nickname, nome, email, senha) FROM stdin;
+PlayerUnnamed	Marcos	marcosviniciusgomesssantos@gmail.com	samsallgood3
+\.
+
 
 --
--- TOC entry 2139 (class 0 OID 49486)
--- Dependencies: 181
+-- TOC entry 2150 (class 0 OID 25539)
+-- Dependencies: 183
+-- Data for Name: postagem; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY postagem (id_post, titulo, texto, data_hora, autor) FROM stdin;
+1	hecarim	criado pelas ilhas das sombras, ele e um centauro criado para causar dor e morte a queles que cruzao o seu caminho	2010-10-10 00:00:00	PlayerUnnamed
+\.
+
+
+--
+-- TOC entry 2149 (class 0 OID 25520)
+-- Dependencies: 182
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+COPY usuario (nickname, nome, email, senha) FROM stdin;
+\.
+
+
 --
--- TOC entry 2022 (class 2606 OID 49490)
+-- TOC entry 2029 (class 2606 OID 25524)
 -- Name: pk_nickname; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -91,7 +126,7 @@ ALTER TABLE ONLY usuario
 
 
 --
--- TOC entry 2024 (class 2606 OID 57684)
+-- TOC entry 2027 (class 2606 OID 25526)
 -- Name: pk_nicknameAdmin; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -100,8 +135,34 @@ ALTER TABLE ONLY admin
 
 
 --
--- TOC entry 2147 (class 0 OID 0)
--- Dependencies: 6
+-- TOC entry 2032 (class 2606 OID 25546)
+-- Name: pk_post; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY postagem
+    ADD CONSTRAINT pk_post PRIMARY KEY (id_post);
+
+
+--
+-- TOC entry 2030 (class 1259 OID 25552)
+-- Name: fki_pk_admin_post; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_pk_admin_post ON postagem USING btree (autor);
+
+
+--
+-- TOC entry 2033 (class 2606 OID 25547)
+-- Name: pk_admin_post; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY postagem
+    ADD CONSTRAINT pk_admin_post FOREIGN KEY (autor) REFERENCES admin(nickname);
+
+
+--
+-- TOC entry 2157 (class 0 OID 0)
+-- Dependencies: 7
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -111,7 +172,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2019-09-25 09:11:47 BRT
+-- Completed on 2019-09-25 12:20:25 BRT
 
 --
 -- PostgreSQL database dump complete
