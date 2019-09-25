3d253050764dcb8e6079d10b7e9b0816f4d1d59c
Kalebe <38252510+kalebe2001@users.noreply.github.com>
2019-09-25 09:15:26 -0300

    Implementado com mais uma tabela

--- GinfFinal.sql
+++ GinfFinal.sql
@@ -2,24 +2,21 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.10 (Ubuntu 10.10-0ubuntu0.18.04.1)
--- Dumped by pg_dump version 10.9 (Ubuntu 10.9-0ubuntu0.18.04.1)
+-- Dumped from database version 9.5.7
+-- Dumped by pg_dump version 9.5.7
 
--- Started on 2019-09-04 22:43:58 -03
+-- Started on 2019-09-25 09:11:47 BRT
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 13043)
+-- TOC entry 1 (class 3079 OID 12395)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -27,7 +24,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2917 (class 0 OID 0)
+-- TOC entry 2148 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -35,16 +32,33 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+SET search_path = public, pg_catalog;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- TOC entry 196 (class 1259 OID 16460)
+-- TOC entry 182 (class 1259 OID 49491)
+-- Name: admin; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE admin (
+    nickname character varying(50) NOT NULL,
+    nome character varying(50),
+    email character varying(50),
+    senha character varying(50)
+);
+
+
+ALTER TABLE admin OWNER TO postgres;
+
+--
+-- TOC entry 181 (class 1259 OID 49486)
 -- Name: usuario; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.usuario (
+CREATE TABLE usuario (
     nickname character varying(20) NOT NULL,
     nome character varying(80),
     email character varying(40),
@@ -52,32 +66,62 @@ CREATE TABLE public.usuario (
 );
 
 
-ALTER TABLE public.usuario OWNER TO postgres;
+ALTER TABLE usuario OWNER TO postgres;
 
 --
--- TOC entry 2909 (class 0 OID 16460)
--- Dependencies: 196
--- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 2140 (class 0 OID 49491)
+-- Dependencies: 182
+-- Data for Name: admin; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.usuario (nickname, nome, email, senha) FROM stdin;
-Kns	Kalebe do Nascimento Sousa	kalebeking@gmail.com	senha
-Cuck	Lucas	lucascuck@gmail.com	soucuck
+COPY admin (nickname, nome, email, senha) FROM stdin;
+Macaé	joão	jjj@.com	123
 \.
 
 
 --
--- TOC entry 2787 (class 2606 OID 16464)
--- Name: usuario pk_nickname; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2139 (class 0 OID 49486)
+-- Dependencies: 181
+-- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.usuario
+COPY usuario (nickname, nome, email, senha) FROM stdin;
+EA	GuiGO111	gui@gmail.com	joystich
+\.
+
+
+--
+-- TOC entry 2022 (class 2606 OID 49490)
+-- Name: pk_nickname; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY usuario
     ADD CONSTRAINT pk_nickname PRIMARY KEY (nickname);
 
 
--- Completed on 2019-09-04 22:43:58 -03
+--
+-- TOC entry 2024 (class 2606 OID 57684)
+-- Name: pk_nicknameAdmin; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY admin
+    ADD CONSTRAINT "pk_nicknameAdmin" PRIMARY KEY (nickname);
+
+
+--
+-- TOC entry 2147 (class 0 OID 0)
+-- Dependencies: 6
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2019-09-25 09:11:47 BRT
 
 --
 -- PostgreSQL database dump complete
 --
-
