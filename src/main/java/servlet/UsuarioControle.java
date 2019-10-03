0706167bb4d1670270bc7f487d0b29c7c92b1ec3
Kalebe <38252510+kalebe2001@users.noreply.github.com>
2019-10-03 10:23:55 -0300

    agora eu espero que seja o ultimo

---
+++ src/main/java/servlet/UsuarioControle.java
@@ -0,0 +1,66 @@
+
+package servlet;
+
+import com.ginf.ginffinal.Usuario;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+public class UsuarioControle {
+
+    //Função de salvar/atualizar um usuario
+    public static boolean salvar(Usuario usuario){
+        try{
+            Session sessionRecheio;
+            sessionRecheio = HibernateUtil.getSession();
+            Transaction tr = sessionRecheio.beginTransaction();
+            sessionRecheio.saveOrUpdate(usuario);
+            tr.commit();
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }        
+    }
+    
+    //Localiza um usuario pelo id
+    public static Usuario buscar(Integer id)
+    {
+        String idUsuario = id.toString();
+        Session sessionRecheio;
+        sessionRecheio = HibernateUtil.getSession();
+        Transaction tr = sessionRecheio.beginTransaction();
+        String hql = "from Usuario u where u.id='"+idUsuario+"'";
+        Usuario usuario = (Usuario)sessionRecheio.createQuery(hql).uniqueResult();
+        tr.commit();
+        return usuario;
+    }
+    
+    //Retorna todos os usuario do sistema
+    public static List<Usuario> listar()
+    {
+        Session sessionRecheio;
+        sessionRecheio = HibernateUtil.getSession();
+        Transaction tr = sessionRecheio.beginTransaction();
+        String hql = "from Usuario u";
+        List<Usuario> lista = (List)sessionRecheio.createQuery(hql).list();
+        tr.commit();
+        return lista;
+    }
+    
+    //Função de apagar um usuario
+    public static boolean deletar(Usuario usuario){
+        try{
+            Session sessionRecheio;
+            sessionRecheio = HibernateUtil.getSession();
+            Transaction tr = sessionRecheio.beginTransaction();
+            sessionRecheio.delete(usuario);
+            tr.commit();
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }        
+    }    
+}
